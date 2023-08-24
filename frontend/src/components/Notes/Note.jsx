b6623e8bb55930f775e605809186958b155df37c
Gabrielende5 <gabrielende53@gmail.com>
2023-08-24 16:40:15 -0300

    note funcionando

--- frontend/src/components/Notes/Note.jsx
+++ frontend/src/components/Notes/Note.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import trash from '../../svgs/trash.svg'
 import edit from '../../svgs/edit.png'
+import "./Note.css"
 
 function Note({n}) {
   return (
@@ -9,10 +10,16 @@ function Note({n}) {
             <h1>{n.title}</h1>
             <div className='desc'>{n.description}</div>
         </div>
-        <div className='div-svgs'>
-            <div><img className="svgs" src={trash}/></div>
-            <div><img className="svgs-edit" src={edit}/></div>
+        <div className='noteRodape'>
+            <div className='dataNote'>
+                {n.date}
+            </div>
+            <div className='div-svgs'>
+                <div><img className="svgs" src={trash}/></div>
+                <div><img className="svgs-edit" src={edit}/></div>
+            </div>
         </div>
+       
         </div>
   )
 }
