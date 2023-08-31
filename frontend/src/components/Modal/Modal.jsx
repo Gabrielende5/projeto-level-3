c9ff3b739a4b536d8690fd75db88cf4105c3037a
Gabrielende5 <gabrielende53@gmail.com>
2023-08-31 17:14:52 -0300

    adicionar coisas

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import "./Modal.css"
 import {AiFillCloseCircle} from 'react-icons/ai'
-function Modal() {
+function Modal(props) {
   return (
     <div className='fundo'>
-        <AiFillCloseCircle width={50} color='white'/>
+      <div className="closeModal" onClick={()=> props.fecharOModal()}>
+        <AiFillCloseCircle size={40} color='white'/>
+      </div>
+      <div>
+        <form className='form'>
+          <label htmlFor="title">Titulo</label>
+          <input type='text' placeholder='Titulo da Anotação' name='title' id='title'></input>
+          <label htmlFor="description">Descrição</label>
+          <input type='text' placeholder='Descrição da Anotação' name='description' id='description' ></input>
+          <button>Cadastrar</button>
+        </form>
+      </div>
     </div>
   )
 }
